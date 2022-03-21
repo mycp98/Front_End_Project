ef813fa9a4254abc4ed1fd816bdea827c6d03e76
Cristian Rosca <cristianrrosca@gmail.com>
2022-03-21 19:54:26 +0000

    style Activity components

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -4,7 +4,9 @@ const Activity = ({activity}) => {
         <>
         <div className="activityNode">
             <h3>{activity.name}</h3>
+            <h4>Description: </h4>
             <p>{activity.description}</p>
+            <h4>Venue: </h4>
             <p>{activity.venue.name}</p>
             <button className="btn" id="book-activity-btn">Book Activity</button>
             

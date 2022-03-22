8417087e53f7723f121d48e57136781300978bd7
Cristian Rosca <cristianrrosca@gmail.com>
2022-03-22 13:37:23 +0000

    add sign in form

--- src/components/UserIdForm.js
+++ src/components/UserIdForm.js
@@ -1,58 +1,77 @@
 import User from "./User";
-const UserIdForm = ({users}) => {
-    const [errorMessages, setErrorMessages] = useState({});
-    const [isSubmitted, setIsSubmitted] = useState(false);
+import { useState } from "react";
 
-    // Generate JSX code for error message
-    const renderErrorMessage = (name) =>
-        name === errorMessages.name && (
-        <div className="error">{errorMessages.message}</div>
+const UserIdForm = ({users}) => {
+
+    // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const errors = {
+    uname: "invalid email",
+    pass: "invalid phone number"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    let { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = users.find((user) => user.email === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.phoneNumber !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
     );
 
-    const database = users.map((user) => {
-        
-        return <User user={user} key={user.id} />
-    })
-
-    // for (const user of database) {
-    //     user.password = "password"
-    // }
-    const errors = {
-        uname: "invalid username",
-        pass: "invalid password"
-      };
-
-      const handleSubmit = (event) => {
-        //Prevent page reload
-        event.preventDefault();
-      
-        var { uname, pass } = document.forms[0];
-      
-        // Find user login info
-        const userData = database.find((user) => user.email === uname.value);
-      
-        // Compare user info
-        if (userData) {
-          if (userData.password !== "password") {
-            // Invalid password
-            setErrorMessages({ name: "pass", message: errors.pass });
-          } else {
-            setIsSubmitted(true);
-          }
-        } else {
-          // Username not found
-          setErrorMessages({ name: "uname", message: errors.uname });
-        }
-      };
-
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Email </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Phone Number </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
     return (
         <>
-        
-        
-        
+        <div className="app">
+            <div className="login-form">
+            <div className="title">Sign In</div>
+          {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+        </div>
+    </div>
         </>
     )
 
-}
+    }
 
 export default UserIdForm;

4fb6d16999d395d5c379a3520fbbbec4be66e07c
Marcy Calderon <marcycalderon@hotmail.co.uk>
2022-03-22 11:39:47 +0000

    start user id login functionality

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+const User = ({user}) => {
+    return(
+        <>
+        <h3>{user.name}</h3>
+        </>
+    )
+}
+
+export default User;
--- src/components/UserIdForm.js
+++ src/components/UserIdForm.js
@@ -1,7 +1,54 @@
-const UserIdForm = () => {
+import User from "./User";
+const UserIdForm = ({users}) => {
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    // Generate JSX code for error message
+    const renderErrorMessage = (name) =>
+        name === errorMessages.name && (
+        <div className="error">{errorMessages.message}</div>
+    );
+
+    const database = users.map((user) => {
+        
+        return <User user={user} key={user.id} />
+    })
+
+    // for (const user of database) {
+    //     user.password = "password"
+    // }
+    const errors = {
+        uname: "invalid username",
+        pass: "invalid password"
+      };
+
+      const handleSubmit = (event) => {
+        //Prevent page reload
+        event.preventDefault();
+      
+        var { uname, pass } = document.forms[0];
+      
+        // Find user login info
+        const userData = database.find((user) => user.email === uname.value);
+      
+        // Compare user info
+        if (userData) {
+          if (userData.password !== "password") {
+            // Invalid password
+            setErrorMessages({ name: "pass", message: errors.pass });
+          } else {
+            setIsSubmitted(true);
+          }
+        } else {
+          // Username not found
+          setErrorMessages({ name: "uname", message: errors.uname });
+        }
+      };
 
     return (
         <>
+        
+        
         
         </>
     )

4a058e99fb4ba423d6f2e60c96967db44554e164
Marcy Calderon <marcycalderon@hotmail.co.uk>
2022-03-24 14:39:22 +0000

    remove h4 venue tag

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -6,7 +6,7 @@ const Activity = ({activity}) => {
             <h3>{activity.name}</h3>
             <h4>Description: </h4>
             <p>{activity.description}</p>
-            <h4>Venue: </h4>
+            {/* <h4>Venue: </h4> */}
             {/* <p>{activity.venue.name}</p> */}
             <button className="btn" id="book-activity-btn">Book Activity</button>
             
